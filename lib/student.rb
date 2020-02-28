2dc371b47e1b879738f8d31052270075098229b6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 01:13:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,9 +101,7 @@ class Student
     WHERE grade = 10
     LIMIT ?
     SQL
-    DB[:conn].execute(sql,x).collect do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql, x)
   end
 
   def self.first_student_in_grade_10
