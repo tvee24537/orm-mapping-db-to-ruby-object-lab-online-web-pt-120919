cfc88662b84f11c15283d0ec36ff01ee057ef7dd
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 01:19:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.count_all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL 
     SELECT * 
     FROM students 
