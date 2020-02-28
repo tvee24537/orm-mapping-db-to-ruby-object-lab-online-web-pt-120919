f101f47dab3f67867947ffd18c1a47a60ea53544
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 00:41:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
     # self.new is the same as running Song.new
     new_student = self.new  
     new_student.id = row[0]
-    new_student.name =  row[1]
+    new_student.name = row[1]
     new_student.grade = row[2]
     new_student
   end
