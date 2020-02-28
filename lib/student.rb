7f8a3a3ca0f1db18f5c02db44b90e61f76df69de
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 00:42:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,8 @@ class Student
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first    
+    student_row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
   end
   
   def save
