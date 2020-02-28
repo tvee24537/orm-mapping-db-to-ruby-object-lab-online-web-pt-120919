b8db89a1a068afe59aafc764bc07df23d0a8adc6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 00:53:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,12 +71,16 @@ class Student
   
   def self.count_all_students_in_grade_9
     sql = "SELECT * FROM students WHERE grade = 9"
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
     sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_x_students_in_grade_10(x)
