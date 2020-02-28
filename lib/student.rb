7259806dc584f0456d55f1de8269536a99a513b7
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 00:51:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first  
   end
